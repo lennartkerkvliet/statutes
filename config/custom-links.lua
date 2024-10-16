5f70fdb31f4c62ea9430a586ed9351f9dc97e9f0
Kai Pischke <kai.pischke@cs.ox.ac.uk>
2024-10-17 00:10:05 +0100

    updated filter

--- config/custom-links.lua
+++ config/custom-links.lua
@@ -1,15 +1,21 @@
 function Link(el)
-    local link_text = pandoc.utils.stringify(el.content)
-    local target_url = el.target:gsub("#", "")
-  
-    -- Check if the link text is "article" or "rule"
-    if link_text == "article" or link_text == "rule" then
-      -- Use \customref, but escape special characters properly
-      -- Double braces {{ and }} are used to prevent interpretation issues
-      return pandoc.RawInline("latex", "\\customref{" .. link_text:gsub("^%l", string.upper) .. "}{" .. target_url .. "}")
-    end
-  
-    -- Return the original link for any other text
-    return el
+  local link_text = pandoc.utils.stringify(el.content)
+  local target_url = el.target:gsub("#", "")
+
+  -- Check if the link text is "article" or "rule"
+  if link_text == "article" or link_text == "rule" then
+    -- Use \customref, but escape special characters properly
+    -- Double braces {{ and }} are used to prevent interpretation issues
+    return pandoc.RawInline("latex", "\\customref{" .. link_text:gsub("^%l", string.upper) .. "}{" .. target_url .. "}")
   end
   
+  -- Check if the link text is "article" or "rule"
+  if link_text == "short-article" or link_text == "short-rule" then
+    -- Use \customref, but escape special characters properly
+    -- Double braces {{ and }} are used to prevent interpretation issues
+    return pandoc.RawInline("latex", "\\shortcustomref{" .. link_text:gsub("^%l", string.upper) .. "}{" .. target_url .. "}")
+  end
+
+  -- Return the original link for any other text
+  return el
+end
