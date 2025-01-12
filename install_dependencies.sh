9583c3e3712708fb839ccaee47dae367c454539e
Kai Pischke <kaipischke@sann176035.st-annes.ox.ac.uk>
2025-01-12 18:24:07 +0000

    Improved netlify workflow

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install texlive-xetex
-echo "Installing texlive-xetex..."
+# Install TeX Live (minimal)
+echo "Installing TeX Live..."
 mkdir -p texlive && cd texlive
 wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar -xzf install-tl-unx.tar.gz --strip-components=1
@@ -11,8 +11,10 @@ cd ..
 
 # Install required LaTeX packages
 echo "Installing required LaTeX packages..."
-tlmgr install xetex fontspec xunicode xltxtra graphicx fancyhdr titlesec tocloft
-tlmgr install FiraSans FiraMono
+texlive/bin/x86_64-linux/tlmgr install \
+  xetex fontspec xunicode xltxtra graphicx fancyhdr titlesec tocloft
+texlive/bin/x86_64-linux/tlmgr install FiraSans FiraMono
 
 # Verify xelatex installation
-xelatex --version || { echo "xelatex installation failed!"; exit 1; }
+echo "Verifying xelatex installation..."
+texlive/bin/x86_64-linux/xelatex --version || { echo "xelatex installation failed!"; exit 1; }
