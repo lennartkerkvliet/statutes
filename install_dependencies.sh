a6ec1cd98d10bce3a0e424b7a08c47bd607eb01e
Kai Pischke <kaipischke@sann176035.st-annes.ox.ac.uk>
2025-01-12 18:38:51 +0000

    Improved netlify workflow

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,10 +5,9 @@ echo "Installing TeX Live..."
 mkdir -p texlive && cd texlive
 wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar -xzf install-tl-unx.tar.gz --strip-components=1
-./install-tl --profile=../config/texlive.profile
+echo "I" | ./install-tl --profile=../config/texlive.profile
 export PATH=$(pwd)/bin/x86_64-linux:$PATH
 cd ..
 
 # Verify xelatex installation
-echo "Verifying xelatex installation..."
 texlive/bin/x86_64-linux/xelatex --version || { echo "xelatex installation failed!"; exit 1; }
